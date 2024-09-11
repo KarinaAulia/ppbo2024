06510ac03a0e98f9d9f6d03eea7b9299c826e98b
KarinaAulia <neomacapella@gmail.com>
2024-09-11 15:36:42 +0700

    Praktikum 5B sampai 5E Penggunaan Require dan Include

--- empat.php
+++ empat.php
@@ -1,62 +1,26 @@
 <?php
 
-class Author {
-    public string $name;
-    public string $description;
+require_once 'Author.php';
+require_once 'Book.php';
+require_once 'Publisher.php';
 
-    public function show(array $type) : array {
-        $hasil = [];
+$author = new Author();
+$author->name = "J.K. Rowling";
+$author->description = "Penulis buku";
 
-        foreach ($type as $key) {
-            if ($key === "name") {
-                $hasil["name"] = $this->name;
-            } elseif ($key === "description") {
-                $hasil["descriction"] = $this->description;
-            } else {
-                $hasil["error"] = "Tipe $key tidak ada yang sesuai.";
-            }
-        }
+$book = new Book();
+$book->ISBN = 0747542155;
+$book->title ="Harry Potter dan Tawanan Azkaban";
+$book->description = "Menceritakan Harry Potter pada tahun ketiganya di sekolah sihir Hogwarts ";
+$book->category = "Novel";
+$book->language = "Indonesia";
+$book->numberOfPage = 544;
+$book->author = $author->name;
+$book->publisher = $publisher->name;
 
-        return $hasil;
-    }
-}
+$publisher = new Publisher();
+$publisher->name = "Gramedia Pustaka Utama";
+$publisher->address = "Jakarta";
+$publisher->setPhone("02153650110");
 
-
-class Book {
-    public int $ISBN;
-    public string $title;
-    public string $description;
-    public string $category;
-    public string $language;
-    public int $numberOfPage;
-    public string $author;
-    public string $publisher;
-
-    public function showAll(): array {
-        return ["ISBN" => $this->ISBN,"Title"=>$this->title,"Description"=>$this->description,"Category"=>$this->category,"Language"=>$this->language,"Number of Page"=>$this->numberOfPage,"Author"=>$this->author,"Publisher"=>$this->publisher];
-    }
-
-    public function detail($ISBN): array {
-        if ($this->ISBN == $ISBN) {
-            return $this->showAll();
-        }
-    }
-}
-
-
-class Publisher {
-    public string $name;
-    public string $address;
-    private string $phone;
-
-    public function setPhone($int) {
-        return $this->phone = $int;
-    }
-
-    public function getPhone(): int {
-        return $this->phone;
-    }
-
-
-}
-
+print_r($book->showAll());
